5eee56d307188e28519fd35ec2e543845b034b7d
jandersen770 <jandersen770@gmail.com>
2020-12-01 18:49:23 -0500

    Update

---
+++ snowflake.js
@@ -0,0 +1,8 @@
+class Snowflake {
+    constructor(W, H, mp){
+        this.x = Math.random() * W, //x-coordinate
+        this.y = Math.random() * H, //y-coordinate
+        this.r = Math.random() * 4 + 1, //radius
+        this.d = Math.random() * mp //density
+    }
+}
